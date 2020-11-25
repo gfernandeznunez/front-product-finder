ab1a7e8bebc735d7b4ee31bd192dc31717681b01
gonzalo <gonzalo.fernandez@tide.cl>
2020-11-25 16:30:49 -0300

    test actualización archivos de config

--- src/server.js
+++ src/server.js
@@ -5,11 +5,9 @@ const PORT = process.env.PORT || '8080'
 const app = express();
 
 app.use(express.static(__dirname+'/dist/ng-blog'));
-//app.use(express.static(__dirname + './dist/expense-app'));
-/*
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
 });
-*/
+
 //app.listen(process.env.PORT || 4200);
 app.set("port", PORT);
